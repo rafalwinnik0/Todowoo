0a3881268705d27fc27f0246a16d5a1a3184b9b3
rafal winnik <rafalwinnik@o2.pl>
2023-08-11 15:09:59 +0200

    last commit

--- todo/views.py
+++ todo/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
 from .models import Todo
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request, 'todo/home.html')
 
@@ -29,6 +30,11 @@ def currenttodos(request):
     todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'todo/currenttodos.html',{'todos':todos})
 
+@login_required
+def completedtodos(request):
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=False)
+    return render(request, 'todo/currenttodos.html',{'todos':todos})
+
 def loginuser(request):
     if request.method == 'GET':
         return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
@@ -39,6 +45,7 @@ def loginuser(request):
         else:
             login(request, user)
             return redirect('currenttodos')
+@login_required
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
@@ -57,6 +64,7 @@ def createtodo(request):
         except ValueError:
             return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error':'Bad data entered'})
 
+@login_required
 def viewtodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk,  user=request.user)
     if request.method == "GET":
@@ -70,9 +78,17 @@ def viewtodo(request, todo_pk):
         except ValueError:
             return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form, 'error':'Bad info'})
 
+@login_required
 def completetodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == "POST":
         todo.datecompleted = timezone.now()
         todo.save()
+        return redirect('currenttodos')
+
+@login_required
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == "POST":
+        todo.delete()
         return redirect('currenttodos')
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -25,7 +25,9 @@ urlpatterns = [
     path("login/", views.loginuser, name='loginuser'),
     path("logout/", views.logoutuser, name='logoutuser'),
     path("current/", views.currenttodos, name='currenttodos'),
+    path("completed/", views.completedtodos, name='completedtodos'),
     path("create/", views.createtodo, name='createtodo'),
     path("todo/<int:todo_pk>", views.viewtodo, name='viewtodo'),
     path("todo/<int:todo_pk>/complete", views.completetodo, name='completetodo'),
+    path("todo/<int:todo_pk>/delete", views.deletetodo, name='deletetodo'),
 ]
